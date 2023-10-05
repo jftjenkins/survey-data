bbe996dff376b74bfd151ebf24d733e4d6efa045
jftjenkins <>
2023-10-05 11:07:43 +0000

    Initial Commit: set up basic structure of Python script.

--- run.py
+++ run.py
@@ -1,17 +1,31 @@
+import gspread
+from oauth2client.service_account import ServiceAccountCredentials
 import pandas as pd
+import matplotlib.pyplot as plt
 
 
-# Function to import survey data from a file
-def import_survey_data(file_path):
+# Function to authenticate with Google Sheets API
+def authenticate_google_sheets():
 
 
-# Function to analyze survey data
-def analyze_survey_data(survey_data):
+# Function to retrieve data from a specific Google Sheet
+def get_data_from_google_sheet(client, sheet_name):
 
 
-# Function to export analysis results to a file
-def export_results(analysis_results, output_file_path):
+# Function to analyze the data (total students, favorite subjects, club counts)
+def analyze_data(df):
 
 
-# Main function to orchestrate the process
+# Function to plot the data (gender distribution, favorite subjects distribution)
+def plot_data(df):
+
+
+# Function to display data and analysis results in the console
+def display_results(df, total_students, favorite_subjects, club_counts):
+
+
+# Main function that orchestrates the workflow
 def main():
+
+
+# Entry point of the script
